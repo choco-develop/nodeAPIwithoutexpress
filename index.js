22514370b4983c424b8064646ef64eb7194a755b
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2022-09-15 17:32:51 +0100

    add Country filter on the GET

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const server = http.createServer((req, res) => {
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(data)
             })
+        } else if (params.get('country')) {
+            fs.readFile('./singers.json', 'utf8', (err, data) => {
+                if (err) throw err;
+                j = JSON.parse(data).filter((e) => e.country === params.get('country'))
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(j))
+            })
+        } else {
+            res.statusCode = 403
+            res.end('Use country parameters please')
         }
     } else {
         figlet('404!', (err, data) => {
