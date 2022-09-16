59b4ba566ec49b56109b8dbf9999f2700f1c16c3
choco-develop <onoigboriaose@gmail.com>
2022-09-16 13:40:59 +0100

    tasks except endpoint and crud done

--- index.js
+++ index.js
@@ -25,11 +25,38 @@ const server = http.createServer((req, res) => {
                 res.writeHead(200, { 'Content-Type': 'application/json' });
                 res.end(JSON.stringify(j))
             })
-        } else {
+           
+        } 
+        else if(params.get('name')){
+            fs.readFile('./singers.json', 'utf8', (err, data) => {
+                if (err) throw err;
+                j = JSON.parse(data).filter((e) => e.name === params.get('name'))
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(j))
+            })
+        }
+        else if(params.get('songName')){
+            fs.readFile('./singers.json', 'utf8', (err, data) => {
+                if (err) throw err;
+                j = JSON.parse(data).filter((e) => e.songName === params.get('songName'))
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                res.end(JSON.stringify(j))
+            })
+        }
+        else if(params.get('album')){
+            fs.readFile('./singer.json','utf8',(err,data) => {
+                if (err) throw err;
+                j=JSON.parse(data).filter((e)=>e.album===params.get('album'))
+                res.writeHead(200,{ 'Content-Type': 'application/json' });
+                res.end(JSON(j))
+            })
+        }
+        else {
             res.statusCode = 403
             res.end('Use country parameters please')
         }
-    } else {
+    } 
+    else {
         figlet('404!', (err, data) => {
             res.statusCode = 404
             res.end(data)
